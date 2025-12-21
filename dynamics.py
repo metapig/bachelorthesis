f64fc428635280ddbb00c454fbc50d5b2bb9d060
metapig <134955810+metapig@users.noreply.github.com>
2025-12-21 19:02:52 +0800

    Add files via upload

--- dynamics.py
+++ dynamics.py
@@ -25,21 +25,34 @@ class Wheels:
 
         self.wheelbase = 2.5  # m, distance between front and rear axles
         self.track_width = 1.5  # m, distance between left and right wheels
-        
+
+        if self.suffix == 'fl':
+            self.x = self.wheelbase / 2
+            self.y = self.track_width / 2
+        if self.suffix == 'fr':
+            self.x = self.wheelbase / 2
+            self.y = -self.track_width / 2
+        if self.suffix == 'rl':
+            self.x = -self.wheelbase / 2
+            self.y = self.track_width / 2
+        if self.suffix == 'rr':
+            self.x = -self.wheelbase / 2
+            self.y = -self.track_width / 2 
+
         self.omega = ca.SX.sym('omega_' + suffix)
         self.T = ca.SX.sym('T_' + suffix)
         self.slip_ratio = ca.SX.sym('slip_ratio_' + suffix)
         self.slip_angle = ca.SX.sym('slip_angle_' + suffix)
 
-        vehicle_velocities = ca.SX.sym('vehicle_velocities_' + suffix, 3)  # [vx, vy, ryp]
+        vehicle_velocities = ca.SX.sym('vehicle_velocities_' + suffix, 3)  # [vx, vy, r]
         delta = ca.SX.sym('delta_' + suffix)
-        A = ca.SX([[1, 0, -1],
-                        [0, 1,  1]])
+        A = ca.SX([[1, 0, -self.y],
+                        [0, 1, self.x]])
         R = ca.SX([[ca.cos(delta),  ca.sin(delta)],
                    [-ca.sin(delta), ca.cos(delta)]])
         self.f = ca.Function('wheel_velocity_' + self.suffix, [vehicle_velocities, delta], [R @ (A @ vehicle_velocities)])
 
-        self.magic_formula_params = ca.SX.system('magic_formula_params_' + suffix, 4,2)  # [B, C, D, E]
+        self.magic_formula_params = ca.SX.sym('magic_formula_params_' + suffix, 4,2)  # [B, C, D, E]
         self.F_x = ca.SX.sym('F_x_' + suffix)
         self.F_y = ca.SX.sym('F_y_' + suffix)
 
@@ -50,11 +63,9 @@ class Wheels:
         vx = vehicle_states[3]
         vy = vehicle_states[4]
         r = vehicle_states[5]
-        yp = self.wheelbase / 2 if self.suffix in ['fl', 'fr'] else -self.wheelbase / 2
-        ryp = r * yp
         delta = vehicle_states[6]
         
-        v_w = self.f(ca.vertcat(vx, vy, ryp), delta)
+        v_w = self.f(ca.vertcat(vx, vy, r), delta)
 
         return v_w[0], v_w[1]
 
